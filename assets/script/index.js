980c3eeba8187ea35f6c87af1beb5e9dbc290431
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-04-02 00:05:58 -0500

    finished project

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,88 @@
-'use strict';
+"use strict";
 
-/* Importing modules */
-import { select, listen, create } from './utils.js';
+import * as utils from './utils.js'
+import words from './words.js';
 
+const displayedWord = utils.getElement('word-display');
+const input = utils.getElement('user-input');
+const startButton = utils.getElement('start-button');
+const timerDuration = 99;
+let timeRemaining = timerDuration;
+let timerInterval;
+const timerElement = utils.getElement('timer');
+const timeRemainingSpan = utils.getElement('time-remaining');
+const scoreElement = utils.getElement('score');
+let currentScore = utils.getElement('current-score');
+let startingScore = 0;
+let shuffledArray = randomizeWords();
+let isVisible = true;
+
+//  Start game
+utils.listen('click', startButton, () => {
+  
+  timerInterval = setInterval(startTimer, 1000);
+  startSound();
+  displayWord();
+});
+
+utils.listen('input', input, () => {
+  checkInput();
+});
+
+function randomizeWords() {
+  return words.sort(() => Math.random() - 0.5);
+}
+
+function startTimer() {
+  timeRemainingSpan.textContent = timeRemaining;
+  timeRemaining--;
+  if (timeRemaining < 0) {
+    clearInterval(timerInterval);
+    timerElement.textContent = 'Time\'s up!';
+    gameEnded();
+  }
+}
+
+function startSound() {
+  const sound = new Audio('./assets/media/alarm.mp3');
+  sound.loop = true;
+  sound.play();
+}
+
+function displayWord() {
+  displayedWord.textContent = shuffledArray[0];
+}
+
+function checkInput() {
+  let gotAMatch = displayedWord.textContent === input.value;
+  console.log(`array length: ${shuffledArray.length}`);
+  if (gotAMatch) { 
+    updateWord();
+    updateScore();
+    clearInput();
+  }
+}function updateWord() {
+  if (shuffledArray.length >= 1) {
+    shuffledArray.shift();
+    displayedWord.textContent = shuffledArray[0];
+  }
+  else {
+    gameEnded();
+    displayedWord.textContent = 'You\'ve beat the game!';
+  }
+}
+
+function updateScore(){
+  currentScore.textContent++;
+}
+
+function clearInput(){
+  input.value = '';
+}
+
+// End Game
+function gameEnded() {
+  input.disabled = true;
+  sound.pause();
+}
 
