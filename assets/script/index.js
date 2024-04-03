205eba8ff8d7cfda803189220eb902bee1c571de
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-04-03 11:01:45 -0500

    made finishing touches ;)

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,19 +3,20 @@
 import * as utils from './utils.js'
 import words from './words.js';
 
+// Variables and Constants
 const displayedWord = utils.getElement('word-display');
 const input = utils.getElement('user-input');
 const startButton = utils.getElement('start-button');
+const stopButton = utils.getElement('stop-button');
 const timerDuration = 99;
 let timeRemaining = timerDuration;
 let timerInterval;
 const timerElement = utils.getElement('timer');
 const timeRemainingSpan = utils.getElement('time-remaining');
-const scoreElement = utils.getElement('score');
 let currentScore = utils.getElement('current-score');
 let startingScore = 0;
 let shuffledArray = randomizeWords();
-let isVisible = true;
+let inputIsVisible = false;
 let sound; // declare sound variable globally
 
 // Start game
@@ -24,6 +25,15 @@ utils.listen('click', startButton, () => {
   startTimer();
   startSound();
   displayWord();
+  if (!inputIsVisible) {
+  displayInputArea();
+  }
+});
+
+// End game
+utils.listen('click', stopButton, () => {
+  resetGame();
+  gameEnded();
 });
 
 utils.listen('input', input, () => {
@@ -60,6 +70,16 @@ function startSound() {
 
 function displayWord() {
   displayedWord.textContent = shuffledArray[0];
+}
+
+function displayInputArea() {
+  input.classList.toggle('visible');
+  inputIsVisible = true;
+}
+
+function removeInputArea() {
+  input.classList.remove('visible');
+  inputIsVisible = false;
 }
 
 function checkInput() {
@@ -104,7 +124,7 @@ function resetGame() {
 
 // End Game
 function gameEnded() {
-  input.disabled = true;
+  removeInputArea();
   if (sound) {
     sound.pause(); // Pause the sound if it's playing
     sound.currentTime = 0; // Reset playback to the beginning
